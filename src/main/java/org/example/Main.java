7c7059345135c4d86d016991490e7e174fcfc4e6
Patricia <patricia.badji89@gmail.com>
2025-02-19 15:45:34 +0100

    changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,17 @@
 package org.example;
+
+import org.example.chainOfResponsability.Generale;
+import org.example.chainOfResponsability.Tenente;
+
 public class Main {
     public static void main(String[] args) {
+
+        Generale generale = new Generale();
+        Tenente tenente = new Tenente();
+        generale.verifica(5);
+        generale.setSuperiore(tenente);
+        tenente.verifica(20000000);
+
 
     }
 }
