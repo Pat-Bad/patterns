220c6d621e1a4b64f199fdc54a1408233c81fcf8
Patricia <patricia.badji89@gmail.com>
2025-02-19 14:29:20 +0100

    changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,6 @@
 package org.example;
-
-import org.example.adapter.Info;
-import org.example.adapter.UserData;
-
-import java.util.Date;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       
+
     }
 }
